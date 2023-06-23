fd3012ec893e10d4063c6b946d9c10366e08e869
Amit <amitkr24031995@gmail.com>
2023-06-24 00:21:23 +0530

    Basics of react js

---
+++ react-practice/src/App.js
@@ -0,0 +1,11 @@
+import L1 from './Components/L1';
+
+function App() {
+  return (
+    <div className="App">
+      <L1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-practice/src/Components/L1.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const L1 = () => {
+  return (
+    <div>
+        <h1>What is React js</h1>
+        <ol>
+            <li>Open source library for building user interface</li>
+            <li>It is not a framework</li>
+            <li>Focus on UI</li>
+            <li>It does not focus on HTTP request and routing. It focus on User interface only</li>
+        </ol>
+    </div>
+  )
+}
+
+export default L1
